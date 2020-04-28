42ab3440d66ae6841e878b2742704df7356942ea
Akiaaa <zakaria.belbsir@gmail.com>
2020-04-28 16:15:40 +0000

    Universal mode deployment

---
+++ pages/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1>Events</h1>
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
+  </div>
+</template>
+<script>
+import EventCard from '../components/EventCard'
+export default {
+  components: {
+    EventCard
+  },
+  head() {
+    return {
+      title: 'Event Listing',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            'Where you can find all the events taking place in your neighborhood'
+        }
+      ]
+    }
+  },
+  async asyncData({ $axios, error }) {
+    try {
+      const { data } = await $axios.get('http://localhost:8080/events')
+      return {
+        events: data
+      }
+    } catch (e) {
+      error({
+        statusCode: 503,
+        message: 'Unable to fetch events at this time. Please try again'
+      })
+    }
+  }
+}
+</script>
